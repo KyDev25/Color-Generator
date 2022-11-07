3f7afcf23595106076e868c4dc4f43ef540a6337
Ky_Dev25 <kylian.barthelemy2003@gmail.com>
2022-11-07 13:57:50 +0100

    version 3 Color Generator

--- Code/index.js
+++ Code/index.js
@@ -6,12 +6,18 @@ const colorGenerator = (r, g, b) => {
   body.style.background = "rgb(" + r + "," + g + "," + b + ")";
 };
 
-setInterval(() => {
+const rgb = () => {
   let r = Math.floor(Math.random() * 255);
   let g = Math.floor(Math.random() * 255);
   let b = Math.floor(Math.random() * 255);
   colorGenerator(r, g, b);
   divRGB.innerHTML = `<span>rgb(${r}, ${g}, ${b})</span>`;
+};
+
+rgb();
+
+setInterval(() => {
+  rgb();
 }, 3000);
 
 divRGB.addEventListener("click", (e) => {
