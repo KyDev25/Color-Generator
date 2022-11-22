afb800a687612b1ae0839ff57f6c53e86da92a8d
Ky_Dev25 <kylian.barthelemy2003@gmail.com>
2022-11-22 15:21:42 +0100

    ajout des commentaires

--- Code/index.js
+++ Code/index.js
@@ -2,10 +2,12 @@ const body = document.querySelector("body");
 const divRGB = document.body.appendChild(document.createElement("div"));
 divRGB.className = "rgb";
 
+//Function to generate rgba color
 const colorGenerator = (r, g, b) => {
   body.style.background = "rgb(" + r + "," + g + "," + b + ")";
 };
 
+//Function to generate random color
 const rgb = () => {
   let r = Math.floor(Math.random() * 255);
   let g = Math.floor(Math.random() * 255);
@@ -16,10 +18,12 @@ const rgb = () => {
 
 rgb();
 
+//Interval to play function every 3 seconds
 setInterval(() => {
   rgb();
 }, 3000);
 
+//When divRGB clicked, copy rgba to clipboard
 divRGB.addEventListener("click", (e) => {
   navigator.clipboard
     .writeText(e.target.textContent)
